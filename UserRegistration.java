a0c52d009ed66e6c1533d55fd4a032cfd9f00308
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-12-18 21:29:33 +0530

    Added:check First Name Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 public class UserRegistration {
-public static void main(String[] args) {
-                System.out.println("Welcome to user registration");
-
-        }
+	private static final String NamePattern = "^[A-Z][a-zA-Z]{2,}$";
+	Scanner scanner = new Scanner(System.in);
+	public void FirstnameValidator() 
+	{
+		System.out.println("Enter First Name");
+		String input = scanner.next();
+		if (input.matches(NamePattern)) {
+			System.out.println("first name sucessfully entered");
+		}
+		else {
+			System.out.println("**Invalid First name**try again**");
+			FirstnameValidator();
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to user registration");
+		UserRegistration user = new UserRegistration();
+		user.FirstnameValidator();
+	}
 }
