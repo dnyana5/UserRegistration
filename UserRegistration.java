33be761122421258c2977c88e75c0d35c298b244
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-12-18 21:36:15 +0530

    Added:check Last Name Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -14,9 +14,21 @@ public class UserRegistration {
 			FirstnameValidator();
 		}
 	}
+	public void LastnameValidator() {
+		System.out.println("Enter the last name:");
+		String input = scanner.next();
+		if (input.matches(NamePattern)) {
+			System.out.println("last name sucessfully entered");
+		}
+		else {
+			System.out.println("**Invalid last name**try again**");
+			LastnameValidator();
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration");
 		UserRegistration user = new UserRegistration();
 		user.FirstnameValidator();
+		user.LastnameValidator();
 	}
 }
