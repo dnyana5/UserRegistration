1224df108ba503dece5897cd82a80ada155522b4
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-12-18 21:47:04 +0530

    Added:check Phone Number Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class UserRegistration {
 	private static final String NamePattern = "^[A-Z][a-zA-Z]{2,}$";
 	private static final String EmailPattern = "^[0-9a-zA-Z]+([.\\-+]?[a-zA-Z0-9]+)?\\@[0-9a-zA-Z]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,})*$";
+	private static final String PhonenumberPattern ="^([\\+]?91)?[6-9]{1}[0-9]{9}$";
 	Scanner scanner = new Scanner(System.in);
 	public void FirstnameValidator() 
 	{
@@ -37,12 +38,24 @@ public class UserRegistration {
 			EmailValidator();
 		}
 	}
+	public void PhoneNumberValidator() {
+		System.out.println("Enter the phone Number:");
+		String input = scanner.next();
+		if (input.matches(PhonenumberPattern)) {
+			System.out.println("phone number sucessfully entered");
+		}
+		else {
+			System.out.println("**Invalid phone number**try again**");
+			PhoneNumberValidator();
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration");
 		UserRegistration user = new UserRegistration();
 		user.FirstnameValidator();
 		user.LastnameValidator();
 		user.EmailValidator();
+		user.PhoneNumberValidator();
 	}
 
 }
