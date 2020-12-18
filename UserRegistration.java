2586b6a2a019382df3d527dab7375a8dc054082b
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-12-18 22:51:10 +0530

    Added:check password Validation Rule4-has exactly one special character

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 public class UserRegistration {
 	private static final String NamePattern = "^[A-Z][a-zA-Z]{2,}$";
 	private static final String EmailPattern = "^[0-9a-zA-Z]+([.\\-+]?[a-zA-Z0-9]+)?\\@[0-9a-zA-Z]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,})*$";
-	private static final String PhonenumberPattern = "^([\\+]?91)?[6-9]{1}[0-9]{9}$";
-	private static final String PasswordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+	private static final String PhonenumberPattern = "^([\\+]?91)?[- ]?[6-9]{1}[0-9]{9}$";
+	private static final String PasswordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?!(?:.*[!@#$%^&*]){2})[a-zA-Z0-9!@#$%^&*]{8,}$";
 	Scanner scanner = new Scanner(System.in);
 	public void FirstnameValidator() 
 	{
 		System.out.println("Enter First Name");
 		String input = scanner.next();
-		if (input.matches(NamePattern)) {
+		if (input.matches(NamePattern)) {	//First name starts with Cap and has minimum 3 characters
 			System.out.println("first name sucessfully entered");
 		}
 		else {
@@ -20,7 +20,7 @@ public class UserRegistration {
 	public void LastnameValidator() {
 		System.out.println("Enter the last name:");
 		String input = scanner.next();
-		if (input.matches(NamePattern)) {
+		if (input.matches(NamePattern)) {	//last name starts with Cap and has minimum 3 characters
 			System.out.println("last name sucessfully entered");
 		}
 		else {
@@ -42,7 +42,7 @@ public class UserRegistration {
 	public void PhoneNumberValidator() {
 		System.out.println("Enter the phone Number:");
 		String input = scanner.next();
-		if (input.matches(PhonenumberPattern)) {
+		if (input.matches(PhonenumberPattern)) {	//Mobile Format -Country code follow by space and 10 digit number
 			System.out.println("phone number sucessfully entered");
 		}
 		else {
@@ -50,10 +50,10 @@ public class UserRegistration {
 			PhoneNumberValidator();
 		}
 	}
-	public void PasswordValidator() {
-		System.out.println("Enter the password:");
-		String input = scanner.next();
-		if (input.matches(PasswordPattern)) {
+	public void PasswordValidator() {			//Rule1 – minimum 8 Characters
+		System.out.println("Enter the password:");	//Rule2 – should have at least 1 Upper case
+		String input = scanner.next();			//Rule3 - should have at least 1 numeric number in password  
+		if (input.matches(PasswordPattern)) {		//Rule4 - has exactly one special character
 			System.out.println("password sucessfully entered");
 		}
 		else {
